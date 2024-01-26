b42de1cd2d2457d9806d25d40f6095f469525e55
LucAnL2 <huynhthaitoan.12c1htk@gmail.com>
2024-01-26 23:05:24 +0700

    Class cua nhanh 1

---
+++ Employee/Nhanh1Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    internal class Nhanh1Class
+    {
+        Console.WriteLine("Thang {0} vua nhap khong hop lw. Tam thoi da ve 1.");
+    }
+}
